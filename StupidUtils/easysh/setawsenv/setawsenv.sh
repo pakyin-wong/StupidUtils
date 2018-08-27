1b58b58069ad087525aed5ee76258dec382bd82f
Wong Pak Yin <wongpakyin@Wongs-MacBook.local>
2018-08-28 00:50:01 +0800

    setawsenv.sh change

--- StupidUtils/easysh/setawsenv/setawsenv.sh
+++ StupidUtils/easysh/setawsenv/setawsenv.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
-while IFS="=" read -r key value; do
-  launchctl setenv $key $value      
+while IFS="=" read -r keyname value; do
+#eval "$keyname=\"\$$keyname\""
+#eval "echo \$$keyname"
+#eval "final_value=\"${!value}\""
+#eval "echo ${!value}"
+#eval "launchctl setenv \$$keyname \"\$final_value\""      
+#eval "echo \"launchctl setenv $keyname \\\"\$final_value\\\"\"" 
+#declare -x "$keyname=${!value}"
+declare -x "$keyname=a"
 done < "$1"
