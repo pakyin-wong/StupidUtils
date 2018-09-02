272564b89e4e8f32be4906ae6e459e3e6c6a609e
pakyin-wong <36594148+pakyin-wong@users.noreply.github.com>
2018-09-02 21:50:36 +0800

    empty update_with_seq_number.sql added find_all_duplicate_rows.sql added without converting into pl/sql

---
+++ StupidUtils/sql/plsql/basic/find_all_duplicate_rows.sql
@@ -0,0 +1,9 @@
+﻿SELECT * 
+FROM member
+LEFT OUTER JOIN (
+
+SELECT MIN( id ) AS id, customername, customertel, memberno
+FROM member
+GROUP BY customername, customertel, memberno
+) AS KeepRows ON member.id = KeepRows.id
+WHERE KeepRows.id IS NULL
