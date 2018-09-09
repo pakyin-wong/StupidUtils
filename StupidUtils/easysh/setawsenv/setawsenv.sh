ac3a717fcf62d5bc2db5d09d3aaa2fffc915f090
Wong Pak Yin <wongpakyin@Wongs-MacBook.local>
2018-09-09 18:51:26 +0800

    shellscript added

--- StupidUtils/easysh/setawsenv/setawsenv.sh
+++ StupidUtils/easysh/setawsenv/setawsenv.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 while IFS="=" read -r keyname value; do
   eval "declare -x \"\$keyname=$value\""
-  unset final_value
-  eval "declare -x \"final_value=$value\""
-  echo $final_value
-
+  eval "echo  \"\$keyname=$value\""
 done < "$1"
