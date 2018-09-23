d626d9220a4f3803b511b7dff0513d452f9d6df4
Pak Yin, Wong <pakyin.wong@link.cuhk.edu.hk>
2018-09-24 01:01:51 +0800

    added weirdcase

--- StupidUtils/easysh/setenv/setenv.sh
+++ StupidUtils/easysh/setenv/setenv.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/sh
 while IFS="=" read -r keyname value; do
-#  echo $keyname
   eval "echo \$keyname=\"$value\""
   eval "export \$keyname=\"$value\""
-#  unset final_value
-#  eval "export final_value=$value"
-#  echo $keyname
-#  echo $final_value
-
 done < "$1"
