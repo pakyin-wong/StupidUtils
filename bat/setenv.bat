a79f2d0bf7b150d28af3721fbac76b79d86ec1f6
Wong, Pak Yin <pakyin.wong@link.cuhk.edu.hk>
2018-10-31 13:59:53 +0800

    setenv.bat updated

--- bat/setenv.bat
+++ bat/setenv.bat
@@ -1,2 +1,8 @@
 @echo referenced from https://lnpcd.blogspot.com/2012/09/19.html
-for /f %i in (%1) do set %i
+@echo referenced from https://zhidao.baidu.com/question/496655777.html
+@echo first param - env variables - example gdriver
+set arg1=%1
+@echo second param - aws variables
+set arg2=%2
+for /f "delims=]" %%i in ( %arg1% ) do set %%i
+for /f "delims=]" %%i in ( %arg2% ) do set %%i
